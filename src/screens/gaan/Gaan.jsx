b5791f1fb9242a170dee4fcf544f6204eb3d9253
mnosumon <mdnuruddin378350@gmail.com>
2025-08-02 00:25:41 +0600

    Shayre apps copy to clipboard and share system added done

--- src/screens/gaan/Gaan.jsx
+++ src/screens/gaan/Gaan.jsx
@@ -1,10 +1,72 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  TextInput,
+  FlatList,
+  Share,
+} from 'react-native';
+import React, { useState } from 'react';
+import Clipboard from '@react-native-clipboard/clipboard';
+import { bani } from '../../components/DummyArr';
 
 const Gaan = () => {
+  const copyToClipboard = text => {
+    Clipboard.setString(text);
+    Alert.alert('অনুলিপি সফল', 'উক্তিটি কপি করা হয়েছে ✅');
+  };
+
+  const shareQuote = async text => {
+    try {
+      const result = await Share.share({
+        message: text,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log('Shared with activity type: ', result.activityType);
+        } else {
+          console.log('Shared successfully');
+        }
+      } else if (result.action === Share.dismissedAction) {
+        console.log('Share dismissed');
+      }
+    } catch (error) {}
+    Alert.alert('ত্রুটি', 'শেয়ার করতে ব্যর্থ ❌');
+    console.error(error.message);
+  };
+
   return (
     <View>
-      <Text>Gaan</Text>
+      <FlatList
+        data={bani}
+        renderItem={({ item }) => (
+          <>
+            <View className="mb-10">
+              <View className="bg-gray-950 w-full h-[30vh] p-5 rounded-s-xl">
+                <Text className="text-white text-xl text-center">
+                  {item.text}
+                </Text>
+              </View>
+              <View className="flex flex-row justify-between items-center bg-blue-400 p-5 rounded-e-xl">
+                <TouchableOpacity
+                  onPress={() => copyToClipboard(item.text)}
+                  className="bg-red-600 px-4 py-2 rounded-lg"
+                >
+                  <Text className="text-white font-bold">কপি করুন</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => shareQuote(item.text)}
+                  className="bg-green-600 px-4 py-2 rounded-lg"
+                >
+                  <Text className="text-white font-bold">শেয়ার করুন</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </>
+        )}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };
--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,28 +1,37 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, ImageBackground } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import HomeBg from '../../assets/image/homeBg.jpg';
 
 const Home = () => {
   const navigation = useNavigation();
   return (
-    <View className="w-full p-5 bg-slate-400 h-screen">
-      <View className="flex flex-row flex-wrap gap-5 w-full py-10">
-        <View className="w-[47.5%] bg-orange-400 rounded-lg p-3 h-48 flex items-center justify-center">
-          <TouchableOpacity onPress={() => navigation.navigate('Gaan')}>
-            <Text className="text-3xl font-bold text-[#2d2d2d]">Gozol</Text>
-          </TouchableOpacity>
-        </View>
-        <View className="w-[47.5%] bg-[#4d0066] rounded-lg p-3 h-48 flex items-center justify-center">
-          <Text className="text-3xl font-bold text-[#FFF]">Gozol</Text>
-        </View>
-        <View className="w-[47.5%] bg-[#a1a475] rounded-lg p-3 h-48 flex items-center justify-center">
-          <Text className="text-3xl font-bold text-[#FFF]">Gozol</Text>
-        </View>
-        <View className="w-[47.5%] bg-[#de27d7] rounded-lg p-3 h-48 flex items-center justify-center">
-          <Text className="text-3xl font-bold text-[#2d2d2d]">Gozol</Text>
+    <ImageBackground source={HomeBg} resizeMode="cover">
+      <View className="w-full p-5 h-screen">
+        <View className="flex flex-row flex-wrap gap-5 w-full py-10">
+          <View className="w-[47%] bg-orange-400 rounded-lg p-3 h-48 flex items-center justify-center">
+            <TouchableOpacity onPress={() => navigation.navigate('Gaan')}>
+              <Text className="text-3xl font-bold text-[#2d2d2d]">Gaan</Text>
+            </TouchableOpacity>
+          </View>
+          <View className="w-[47%] bg-[#4d0066] rounded-lg p-3 h-48 flex items-center justify-center">
+            <TouchableOpacity onPress={() => navigation.navigate('Gozol')}>
+              <Text className="text-3xl font-bold text-[#FFF]">Gozol</Text>
+            </TouchableOpacity>
+          </View>
+          <View className="w-[47%] bg-[#a1a475] rounded-lg p-3 h-48 flex items-center justify-center">
+            <TouchableOpacity onPress={() => navigation.navigate('Kobita')}>
+              <Text className="text-3xl font-bold text-[#2d2d2d]">Kobita</Text>
+            </TouchableOpacity>
+          </View>
+          <View className="w-[47%] bg-[#de27d7] rounded-lg p-3 h-48 flex items-center justify-center">
+            <TouchableOpacity onPress={() => navigation.navigate('Shayre')}>
+              <Text className="text-3xl font-bold text-[#2d2d2d]">Shayre</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
